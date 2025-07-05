d5e2d403037d239ea2ceca999f5476a32412ec00
Marcelo F. Cândido <fcandido.marcelo@gmail.com>
2025-07-04 23:31:48 -0300

    Check for record on zone

--- client/cloudflare.go
+++ client/cloudflare.go
@@ -7,6 +7,7 @@ import (
 	"sync"
 
 	"github.com/cloudflare/cloudflare-go/v3"
+	"github.com/cloudflare/cloudflare-go/v3/dns"
 	"github.com/cloudflare/cloudflare-go/v3/option"
 	"github.com/cloudflare/cloudflare-go/v3/zones"
 )
@@ -32,6 +33,8 @@ func GetSingletonClient() (*cloudflare.Client) {
 }
 
 func GetZoneIDByName(zoneName string) (string, error) {
+	fmt.Printf("Retrieving zone ID for zone name: %s\n", zoneName)
+
 	client := GetSingletonClient()
 
 	page, err := client.Zones.List(context.TODO(), zones.ZoneListParams{
@@ -47,3 +50,26 @@ func GetZoneIDByName(zoneName string) (string, error) {
 
 	return page.Result[0].ID, nil
 }
+
+func DoesRecordExistOnZone(zoneID, recordName string) (bool, error) {
+	fmt.Printf("Checking if record '%s' exists on zone '%s'\n", recordName, zoneID)
+
+	client := GetSingletonClient()
+
+	page, err := client.DNS.Records.List(context.TODO(), dns.RecordListParams{
+		ZoneID: cloudflare.F(zoneID),
+		Name: cloudflare.F(recordName),
+	})
+	if err != nil {
+		return false, fmt.Errorf("failed to list DNS records: %w", err)
+	}
+
+	for _, record := range page.Result {
+		if record.Name == recordName {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
+
--- cmd/yaca/main.go
+++ cmd/yaca/main.go
@@ -12,11 +12,18 @@ func main() {
   args := utils.ParseArgs()
 
   zoneID, err := client.GetZoneIDByName(args.ZoneName)
-  if err != nil {
-    panic(err)
-  }
+  utils.PanicOnError(err)
 
 	fmt.Printf("Zone ID: %+v\n", zoneID)
+
+  recordExists, err := client.DoesRecordExistOnZone(zoneID, args.Record)
+  utils.PanicOnError(err)
+
+  if recordExists {
+    fmt.Printf("Record %s already exists on zone %s.\n", args.Record, args.ZoneName)
+  } else {
+    fmt.Printf("Record %s does not exist on zone %s.\n", args.Record, args.ZoneName)
+  }
 
   // DoesDomainExistOnZone()
   // if true
