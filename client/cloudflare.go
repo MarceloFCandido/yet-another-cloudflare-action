bb4bb4ddb9ee850b009d6557d063648f86a807f7
Marcelo F. Cândido <fcandido.marcelo@gmail.com>
2025-07-04 22:37:31 -0300

    Turn Cloudflare client instance into a Singleton

--- client/cloudflare.go
+++ client/cloudflare.go
@@ -1,17 +1,30 @@
 package client
 
 import (
+	"fmt"
 	"os"
+	"sync"
 
 	"github.com/cloudflare/cloudflare-go/v3"
 	"github.com/cloudflare/cloudflare-go/v3/option"
 )
 
-func GetClient() (*cloudflare.Client) {
-  client := cloudflare.NewClient(
-		option.WithAPIEmail(os.Getenv("CLOUDFLARE_API_EMAIL")),
-		option.WithAPIToken(os.Getenv("CLOUDFLARE_API_TOKEN")),
-	)
+var once sync.Once
+var client *cloudflare.Client
+
+func GetSingletonClient() (*cloudflare.Client) {
+	if client == nil {
+		once.Do(func() {
+			fmt.Println("Initializing singleton Cloudflare client...")
+
+			client = cloudflare.NewClient(
+				option.WithAPIEmail(os.Getenv("CLOUDFLARE_API_EMAIL")),
+				option.WithAPIToken(os.Getenv("CLOUDFLARE_API_TOKEN")),
+			)
+		})
+	} else {
+		fmt.Println("Using existing Cloudflare client instance.")
+	}
 
 	return client
 }
--- cmd/yaca/main.go
+++ cmd/yaca/main.go
@@ -10,7 +10,18 @@ func main() {
 
   args := utils.ParseArgs()
 
-  client := client.GetClient()
+  client := client.GetSingletonClient()
+
+  // zoneID, err = client.GetZoneIDByName(args.ZoneName)
+
+  // page, err := client.Zones.List(context.TODO(), zones.ZoneListParams{
+  //   Name: cloudflare.F(args.ZoneName),
+  // })
+	// if err != nil {
+	// 	panic(err.Error())
+	// }
+
+	// fmt.Printf("%+v\n", page.Result[0].ID)
 
   // GetZoneID()
   // DoesDomainExistOnZone()
