b7326662f1da9b42a257c00de3e9447e27415531
Marcelo F. Cândido <fcandido.marcelo@gmail.com>
2025-07-10 15:38:26 -0300

    Keep the exit code logic while allowing tests

--- cmd/yaca/main.go
+++ cmd/yaca/main.go
@@ -10,24 +10,23 @@ import (
 )
 
 var (
-	utilsLoadEnv          = utils.LoadEnv
-	utilsParseArgs        = utils.ParseArgs
-	utilsValidateArgs     = utils.ValidateArgs
-	utilsPanicOnError     = utils.PanicOnError
-	clientGetZoneIDByName = client.GetZoneIDByName
+	utilsLoadEnv                = utils.LoadEnv
+	utilsParseArgs              = utils.ParseArgs
+	utilsValidateArgs           = utils.ValidateArgs
+	utilsPanicOnError           = utils.PanicOnError
+	clientGetZoneIDByName       = client.GetZoneIDByName
 	clientDoesRecordExistOnZone = client.DoesRecordExistOnZone
-	clientUpdateRecordOnZone = client.UpdateRecordOnZone
-	clientCreateRecordOnZone = client.CreateRecordOnZone
-	clientDeleteRecordOnZone = client.DeleteRecordOnZone
-	osExit = os.Exit
+	clientUpdateRecordOnZone    = client.UpdateRecordOnZone
+	clientCreateRecordOnZone    = client.CreateRecordOnZone
+	clientDeleteRecordOnZone    = client.DeleteRecordOnZone
 )
 
-func main() {
+func run() int {
 	utilsLoadEnv()
 
 	args := utilsParseArgs()
-  err := utilsValidateArgs(&args)
-  utilsPanicOnError(err)
+	err := utilsValidateArgs(&args)
+	utilsPanicOnError(err)
 
 	zoneID, err := clientGetZoneIDByName(args.ZoneName)
 	utilsPanicOnError(err)
@@ -37,47 +36,49 @@ func main() {
 	recordID, err := clientDoesRecordExistOnZone(zoneID, args.Record)
 	utilsPanicOnError(err)
 
-  record := models.Record{
-    Record: args.Record,
-    Proxy:  args.Proxy,
-    Target: args.Target,
-    Ttl:    args.Ttl,
-    Type:   args.Type,
-  }
+	record := models.Record{
+		Record: args.Record,
+		Proxy:  args.Proxy,
+		Target: args.Target,
+		Ttl:    args.Ttl,
+		Type:   args.Type,
+	}
 
 	if recordID != "" {
 		fmt.Printf("Record %s exists on zone %s.\n", args.Record, args.ZoneName)
 
-    if !args.Delete {
-      success, err := clientUpdateRecordOnZone(zoneID, recordID, record)
-      utilsPanicOnError(err)
+		if !args.Delete {
+			success, err := clientUpdateRecordOnZone(zoneID, recordID, record)
+			utilsPanicOnError(err)
 
-      if success {
-        fmt.Printf("Record %s updated successfully on zone %s.\n", args.Record, args.ZoneName)
-        osExit(0)
-      }
-    } else {
-      success, err := clientDeleteRecordOnZone(zoneID, recordID, record)
-      utilsPanicOnError(err)
+			if success {
+				fmt.Printf("Record %s updated successfully on zone %s.\n", args.Record, args.ZoneName)
+				return 0
+			}
+		} else {
+			success, err := clientDeleteRecordOnZone(zoneID, recordID, record)
+			utilsPanicOnError(err)
 
-      if success {
-        fmt.Printf("Record %s deleted successfully from zone %s.\n", args.Record, args.ZoneName)
-        osExit(0)
-      }
-    }
+			if success {
+				fmt.Printf("Record %s deleted successfully from zone %s.\n", args.Record, args.ZoneName)
+				return 0
+			}
+		}
 	} else {
 		fmt.Printf("Record %s does not exist on zone %s.\n", args.Record, args.ZoneName)
 
 		success, err := clientCreateRecordOnZone(zoneID, record)
-    utilsPanicOnError(err)
+		utilsPanicOnError(err)
 
-    if success {
-      fmt.Printf("Record %s created successfully on zone %s.\n", args.Record, args.ZoneName)
-      osExit(0)
-    }
+		if success {
+			fmt.Printf("Record %s created successfully on zone %s.\n", args.Record, args.ZoneName)
+			return 0
+		}
 	}
 
-  if os.Getenv("GO_TESTING") != "true" {
-    osExit(1)
-  }
+	return 1
+}
+
+func main() {
+	os.Exit(run())
 }
--- cmd/yaca/main_test.go
+++ cmd/yaca/main_test.go
@@ -8,15 +8,15 @@ import (
 
 // Global mock variables
 var (
-	mockLoadEnvFunc           func() error
-	mockParseArgsFunc         func() models.Args
-	mockValidateArgsFunc      func(*models.Args) error
-	mockPanicOnErrorFunc      func(error)
-	mockGetZoneIDByNameFunc   func(string) (string, error)
+	mockLoadEnvFunc             func() error
+	mockParseArgsFunc           func() models.Args
+	mockValidateArgsFunc        func(*models.Args) error
+	mockPanicOnErrorFunc        func(error)
+	mockGetZoneIDByNameFunc     func(string) (string, error)
 	mockDoesRecordExistOnZoneFunc func(string, string) (string, error)
-	mockUpdateRecordOnZoneFunc func(string, string, models.Record) (bool, error)
-	mockCreateRecordOnZoneFunc func(string, models.Record) (bool, error)
-	mockDeleteRecordOnZoneFunc func(string, string, models.Record) (bool, error)
+	mockUpdateRecordOnZoneFunc  func(string, string, models.Record) (bool, error)
+	mockCreateRecordOnZoneFunc  func(string, models.Record) (bool, error)
+	mockDeleteRecordOnZoneFunc  func(string, string, models.Record) (bool, error)
 )
 
 // Override original functions with mocks
@@ -24,22 +24,25 @@ func init() {
 	utilsLoadEnv = func() error { return mockLoadEnvFunc() }
 	utilsParseArgs = func() models.Args { return mockParseArgsFunc() }
 	utilsValidateArgs = func(args *models.Args) error { return mockValidateArgsFunc(args) }
-	utilsPanicOnError = func(err error) { 
+	utilsPanicOnError = func(err error) {
 		if err != nil {
 			panic(err)
 		}
 	}
 	clientGetZoneIDByName = func(zoneName string) (string, error) { return mockGetZoneIDByNameFunc(zoneName) }
 	clientDoesRecordExistOnZone = func(zoneID, recordName string) (string, error) { return mockDoesRecordExistOnZoneFunc(zoneID, recordName) }
-	clientUpdateRecordOnZone = func(zoneID, recordID string, record models.Record) (bool, error) { return mockUpdateRecordOnZoneFunc(zoneID, recordID, record) }
-	clientCreateRecordOnZone = func(zoneID string, record models.Record) (bool, error) { return mockCreateRecordOnZoneFunc(zoneID, record) }
-	clientDeleteRecordOnZone = func(zoneID, recordID string, record models.Record) (bool, error) { return mockDeleteRecordOnZoneFunc(zoneID, recordID, record) }
+	clientUpdateRecordOnZone = func(zoneID, recordID string, record models.Record) (bool, error) {
+		return mockUpdateRecordOnZoneFunc(zoneID, recordID, record)
+	}
+	clientCreateRecordOnZone = func(zoneID string, record models.Record) (bool, error) {
+		return mockCreateRecordOnZoneFunc(zoneID, record)
+	}
+	clientDeleteRecordOnZone = func(zoneID, recordID string, record models.Record) (bool, error) {
+		return mockDeleteRecordOnZoneFunc(zoneID, recordID, record)
+	}
 }
 
 func TestUpdateRecord(t *testing.T) {
-	var exitCode int
-	osExit = func(code int) { exitCode = code }
-
 	mockLoadEnvFunc = func() error { return nil }
 	mockParseArgsFunc = func() models.Args {
 		return models.Args{
@@ -56,10 +59,16 @@ func TestUpdateRecord(t *testing.T) {
 	mockGetZoneIDByNameFunc = func(zoneName string) (string, error) { return "test-zone-id", nil }
 	mockDoesRecordExistOnZoneFunc = func(zoneID, recordName string) (string, error) { return "test-record-id", nil }
 	mockUpdateRecordOnZoneFunc = func(zoneID, recordID string, record models.Record) (bool, error) { return true, nil }
-	mockCreateRecordOnZoneFunc = func(zoneID string, record models.Record) (bool, error) { return false, errors.New("should not be called") }
-	mockDeleteRecordOnZoneFunc = func(zoneID, recordID string, record models.Record) (bool, error) { return false, errors.New("should not be called") }
+	mockCreateRecordOnZoneFunc = func(zoneID string, record models.Record) (bool, error) {
+		return false,
+		errors.New("should not be called")
+	}
+	mockDeleteRecordOnZoneFunc = func(zoneID, recordID string, record models.Record) (bool, error) {
+		return false,
+		errors.New("should not be called")
+	}
 
-	main()
+	exitCode := run()
 
 	if exitCode != 0 {
 		t.Errorf("Expected exit code 0, got %d", exitCode)
@@ -67,9 +76,6 @@ func TestUpdateRecord(t *testing.T) {
 }
 
 func TestCreateRecord(t *testing.T) {
-	var exitCode int
-	osExit = func(code int) { exitCode = code }
-
 	mockLoadEnvFunc = func() error { return nil }
 	mockParseArgsFunc = func() models.Args {
 		return models.Args{
@@ -85,11 +91,17 @@ func TestCreateRecord(t *testing.T) {
 	mockValidateArgsFunc = func(args *models.Args) error { return nil }
 	mockGetZoneIDByNameFunc = func(zoneName string) (string, error) { return "test-zone-id", nil }
 	mockDoesRecordExistOnZoneFunc = func(zoneID, recordName string) (string, error) { return "", nil }
-	mockUpdateRecordOnZoneFunc = func(zoneID, recordID string, record models.Record) (bool, error) { return false, errors.New("should not be called") }
+	mockUpdateRecordOnZoneFunc = func(zoneID, recordID string, record models.Record) (bool, error) {
+		return false,
+		errors.New("should not be called")
+	}
 	mockCreateRecordOnZoneFunc = func(zoneID string, record models.Record) (bool, error) { return true, nil }
-	mockDeleteRecordOnZoneFunc = func(zoneID, recordID string, record models.Record) (bool, error) { return false, errors.New("should not be called") }
+	mockDeleteRecordOnZoneFunc = func(zoneID, recordID string, record models.Record) (bool, error) {
+		return false,
+		errors.New("should not be called")
+	}
 
-	main()
+	exitCode := run()
 
 	if exitCode != 0 {
 		t.Errorf("Expected exit code 0, got %d", exitCode)
@@ -97,9 +109,6 @@ func TestCreateRecord(t *testing.T) {
 }
 
 func TestDeleteRecord(t *testing.T) {
-	var exitCode int
-	osExit = func(code int) { exitCode = code }
-
 	mockLoadEnvFunc = func() error { return nil }
 	mockParseArgsFunc = func() models.Args {
 		return models.Args{
@@ -111,11 +120,17 @@ func TestDeleteRecord(t *testing.T) {
 	mockValidateArgsFunc = func(args *models.Args) error { return nil }
 	mockGetZoneIDByNameFunc = func(zoneName string) (string, error) { return "test-zone-id", nil }
 	mockDoesRecordExistOnZoneFunc = func(zoneID, recordName string) (string, error) { return "test-record-id", nil }
-	mockUpdateRecordOnZoneFunc = func(zoneID, recordID string, record models.Record) (bool, error) { return false, errors.New("should not be called") }
-	mockCreateRecordOnZoneFunc = func(zoneID string, record models.Record) (bool, error) { return false, errors.New("should not be called") }
+	mockUpdateRecordOnZoneFunc = func(zoneID, recordID string, record models.Record) (bool, error) {
+		return false,
+		errors.New("should not be called")
+	}
+	mockCreateRecordOnZoneFunc = func(zoneID string, record models.Record) (bool, error) {
+		return false,
+		errors.New("should not be called")
+	}
 	mockDeleteRecordOnZoneFunc = func(zoneID, recordID string, record models.Record) (bool, error) { return true, nil }
 
-	main()
+	exitCode := run()
 
 	if exitCode != 0 {
 		t.Errorf("Expected exit code 0, got %d", exitCode)
@@ -133,7 +148,7 @@ func TestGetZoneIDByNameFails(t *testing.T) {
 	mockValidateArgsFunc = func(args *models.Args) error { return nil }
 	mockGetZoneIDByNameFunc = func(zoneName string) (string, error) { return "", errors.New("test error") }
 
-	main()
+	run()
 }
 
 func TestDoesRecordExistOnZoneFails(t *testing.T) {
@@ -148,5 +163,5 @@ func TestDoesRecordExistOnZoneFails(t *testing.T) {
 	mockGetZoneIDByNameFunc = func(zoneName string) (string, error) { return "test-zone-id", nil }
 	mockDoesRecordExistOnZoneFunc = func(zoneID, recordName string) (string, error) { return "", errors.New("test error") }
 
-	main()
+	run()
 }
